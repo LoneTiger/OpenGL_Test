dc78fa5ee60dc57b2313a84afbe77f6a83beb961
LoneTiger <nul>
2021-06-27 19:57:27 -0500

    Add Texture Loading Function

--- OpenGL_Test.cpp
+++ OpenGL_Test.cpp
@@ -8,6 +8,9 @@
 
 using namespace std;
 
+void logSDLError(ostream& os, const string& msg);
+SDL_Texture* loadTexture(const string& file, SDL_Renderer* ren);
+
 int main()
 {
 	// Vars
@@ -16,13 +19,13 @@ int main()
 
 	cout << "Hello bruh" << endl;
 
-	//Initialize SDL2
+	// Initialize SDL2
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		logSDLError(cout, "Video_init");
 		return 1;
 	}
 
-	//Create window
+	// Create window
 	SDL_Window* win = SDL_CreateWindow("OpenGL_Test", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
 	if (win == nullptr) {
 		logSDLError(cout, "Creating window");
@@ -30,7 +33,7 @@ int main()
 		return 1;
 	}
 
-	//Create renderer
+	// Create renderer
 	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (ren == nullptr) {
 		SDL_DestroyWindow(win);
@@ -39,8 +42,8 @@ int main()
 		return 1;
 	}
 
-	//Load image
-	char* basePath = SDL_GetBasePath();
+	// Load image
+	/*char* basePath = SDL_GetBasePath();
 	string baseDir = basePath;
 	SDL_free(basePath);
 	string imagePath = baseDir + "Trollface.bmp";
@@ -52,8 +55,13 @@ int main()
 		SDL_Quit();
 		return 1;
 	}
+	*/
 
-	//Create texture to display the image
+	// Load image to texture
+	SDL_Texture* tex = loadTexture("Trollface.bmp", ren);
+
+	// Create texture to display the image
+	/*
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
 	SDL_FreeSurface(bmp);
 	if (tex == nullptr) {
@@ -63,14 +71,15 @@ int main()
 		SDL_Quit();
 		return 1;
 	}
+	*/
 
-	//Draw the texture
+	// Draw the texture
 	SDL_RenderClear(ren);
 	SDL_RenderCopy(ren, tex, NULL, NULL);
 	SDL_RenderPresent(ren);
 	SDL_Delay(5000);
 
-	//Clean up
+	// Clean up
 	SDL_DestroyTexture(tex);
 	SDL_DestroyRenderer(ren);
 	SDL_DestroyWindow(win);
@@ -82,4 +91,26 @@ int main()
 // Error message logger/printer
 void logSDLError(ostream& os, const string& msg) {
 	os << msg << " error: " << SDL_GetError() << endl;
+}
+
+// Texture loading function
+// Takes path to image and renderer, returns pointer to texture
+SDL_Texture* loadTexture(const string& file, SDL_Renderer* ren) {
+	// Init as nullptr
+	SDL_Texture* texture = nullptr;
+	// Load image
+	SDL_Surface* loadedImage = SDL_LoadBMP(file.c_str());
+	// Make sure we didn't fail to load the image
+	if (loadedImage != nullptr) {
+		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
+		SDL_FreeSurface(loadedImage);
+		//Make sure the texture was created
+		if (texture == nullptr) {
+			logSDLError(cout, "CreateTextureFromSurface");
+		}
+	}
+	else {
+		logSDLError(cout, "LoadBMP");
+	}
+	return texture;
 }
