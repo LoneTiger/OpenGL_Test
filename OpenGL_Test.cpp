ca6aed02cdc8cb682eb9111b991a1a64be85c669
LoneTiger <58406610+LoneTiger@users.noreply.github.com>
2021-07-12 20:46:58 -0500

    Colorful triangle! Also new shader error logger.

--- OpenGL_Test.cpp
+++ OpenGL_Test.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 void logSDLError(ostream& os, const string& msg);
+void logShaderError(ostream& os, const string& msg, int type);
 
 int main()
 {
@@ -19,9 +20,10 @@ int main()
 
 	// Triangle verticies
 	float verticies[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
+		// Coordinates		// Colors
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+		 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 	};
 
 	// Indices
@@ -32,16 +34,19 @@ int main()
 	// Vertex shader (yeah I know this is cursed, it's temporary)
 	const char* vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
+		"layout (location = 1) in vec3 aColor;\n"
+		"out vec3 ourColor;\n"
 		"void main() {\n"
-		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"	gl_Position = vec4(aPos, 1.0);\n"
+		"	ourColor = aColor;"
 		"}\n";
 
 	// Fragment shader
 	const char* fragmentShaderSource = "#version 330 core\n"
 		"out vec4 FragColor;\n"
-		"uniform vec4 ourColor;\n"
+		"in vec3 ourColor;\n"
 		"void main() {\n"
-		"	FragColor = ourColor;\n"
+		"	FragColor = vec4(ourColor, 1.0);\n"
 		"}\n";
 
 
@@ -64,17 +69,18 @@ int main()
 
 	// Time for OpenGL!! :D
 
+	// Set everything up
 	SDL_GLContext context = SDL_GL_CreateContext(win); // Make the context
-	gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress); //???
+	gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress); // GLAD
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE); // Only CORE PROFILE allowed in here
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3); // OpenGL 3
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3); // OpenGL 3.3
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT); // Set OpenGL resolution/viewport thingy
 
-	// Print max number of vertex attributes
+	// Print max number of vertex attributes (I'm curious)
 	int nrAttributes;
 	glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
-	cout << "Mac number of vertex attributes: " << nrAttributes << endl;
+	cout << "Max number of vertex attributes: " << nrAttributes << endl;
 
 	// Create vertex shader object
 	unsigned int vertexShader;
@@ -91,7 +97,7 @@ int main()
 	// If it failed
 	if (!success) {
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		logSDLError(cout, infoLog);
+		logShaderError(cout, infoLog, 0);
 		SDL_GL_DeleteContext(context);
 		SDL_DestroyWindow(win);
 		SDL_Quit();
@@ -108,7 +114,7 @@ int main()
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		logSDLError(cout, infoLog);
+		logShaderError(cout, infoLog, 1);
 		SDL_GL_DeleteContext(context);
 		SDL_DestroyWindow(win);
 		SDL_Quit();
@@ -126,12 +132,16 @@ int main()
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
 	if (!success) {
 		glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
-		logSDLError(cout, infoLog);
+		logShaderError(cout, infoLog, 2);
 		SDL_GL_DeleteContext(context);
 		SDL_DestroyWindow(win);
 		SDL_Quit();
 		return 1;
 	}
+
+	// Delete shader objects now that the program has been set up
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	// VAO
 	unsigned int VAO;
@@ -145,18 +155,17 @@ int main()
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW); // Load the vertex array to the buffer
 
 	// Vertex attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// Color attributes
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	// EBO
 	unsigned int EBO;
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// Delete shader objects now that the program has been set up
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
 
 	// Temp wireframe rendering
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -184,16 +193,10 @@ int main()
 		// OpenGL :D
 
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Red, Green, Blue, Alpha
-		glClear(GL_COLOR_BUFFER_BIT); // I honestly have no idea what this does
-
-		// Update triangle color
-		float timeValue = SDL_GetTicks();
-		float greenValue = (sin(timeValue / 500) / 2.0f + 0.5f);
-		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glClear(GL_COLOR_BUFFER_BIT); // Clear the screen with above color
 
 		// Draw triangle
 		glUseProgram(shaderProgram);
-		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO);
 		//glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -209,7 +212,29 @@ int main()
 	return 0;
 }
 
-// Error message logger/printer
+// SDL Error message logger/printer
 void logSDLError(ostream& os, const string& msg) {
 	os << msg << " error: " << SDL_GetError() << endl;
+}
+
+// OpenGL shader error message logger
+void logShaderError(ostream& os, const string& msg, int type) {
+
+	/*
+	* Types:
+	* 0 - vertex shader
+	* 1 - fragment shader
+	* 2 - program
+	*/
+
+	switch (type) {
+		case 0:
+			os << "Vertex shader error: " << msg << endl;
+			break;
+		case 1:
+			os << "Fragment shader error: " << msg << endl;
+			break;
+		case 2:
+			os << "Shader program error: " << msg << endl;
+	}
 }
